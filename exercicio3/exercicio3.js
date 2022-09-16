773b53fd69b417a72c9f2b7702fe0056d53c5cd1
DKFAEL <rafaelnunesemp@hotmail.com>
2022-09-16 20:57:17 -0300

    primeiros exercicios concluido

---
+++ exercicio3/exercicio3.js
@@ -0,0 +1,9 @@
+let raiz =  Number (prompt("Insira o tamanho do raio aqui"))
+let ValorPi = 3.1415
+let raio = Math.pow ( raiz , 2)
+alert('Área da  circunferência e ' + raio * ValorPi)
+let Perimetro = ValorPi * 2 
+alert ('Perimetro de: ' + Perimetro *raiz) 
+
+
+
